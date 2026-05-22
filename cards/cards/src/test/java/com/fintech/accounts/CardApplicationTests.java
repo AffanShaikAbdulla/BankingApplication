6ffb5da5666851f2b463c5c4fef417377733714f
AffanShaikAbdulla <shaikabdulla0298@gmail.com>
2026-05-22 17:15:32 +0530

    feat: add cards microservice
    - Add card issuance API
    - Add card blocking/unblocking
    - Add transaction limits management

---
+++ cards/cards/src/test/java/com/fintech/accounts/CardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintech.accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
