2de2119e85a0424b176d6b880ebc03885b485e96
AffanShaikAbdulla <shaikabdulla0298@gmail.com>
2026-06-01 21:46:24 +0530

    fix: add missing loans folder from template

--- loans/src/test/java/com/fintech/accounts/AccountsApplicationTests.java
+++ loans/src/test/java/com/fintech/accounts/AccountsApplicationTests.java
@@ -1,10 +1,10 @@
-package com.fintech.loans;
+package com.fintech.accounts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LoansApplicationTests {
+class AccountsApplicationTests {
 
 	@Test
 	void contextLoads() {
