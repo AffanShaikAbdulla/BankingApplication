fff20c42dd9f606ffda82a99d97368cc5cc5c353
AffanShaikAbdulla <shaikabdulla0298@gmail.com>
2026-06-01 11:04:17 +0530

    feat: containerize all microservices with Docker

--- cards/cards/src/test/java/com/fintech/accounts/CardApplicationTests.java
+++ cards/cards/src/test/java/com/fintech/accounts/CardApplicationTests.java
@@ -3,7 +3,9 @@ package com.fintech.accounts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.fintech.cards.CardApplication;
+
+@SpringBootTest(classes = CardApplication.class)
 class CardApplicationTests {
 
 	@Test
