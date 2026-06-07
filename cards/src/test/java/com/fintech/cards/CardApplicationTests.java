13dfcee542ec5d72cac680c6fe91b54b92e181f7
AffanShaikAbdulla <shaikabdulla0298@gmail.com>
2026-06-07 10:50:03 +0530

    feat: integrate Spring Cloud Config Server and migrate services to Spring Boot 3.2.x

--- cards/src/test/java/com/fintech/cards/CardApplicationTests.java
+++ cards/src/test/java/com/fintech/cards/CardApplicationTests.java
@@ -1,15 +1,9 @@
-package com.fintech.accounts;
+package com.fintech.cards;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-<<<<<<< HEAD
-import com.fintech.cards.CardApplication;
-
-@SpringBootTest(classes = CardApplication.class)
-=======
 @SpringBootTest
->>>>>>> main
 class CardApplicationTests {
 
 	@Test
